bd1d30d4cb71580e226744d03d3899de399b1103
kvashnin <kvashnin.semion@yandex.ru>
2025-11-21 20:03:18 +0300

    basdf

--- src/main/java/jdbc/User.java
+++ src/main/java/jdbc/User.java
@@ -1,19 +1,23 @@
 package jdbc;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
 public class User {
     private Long id;
     private String firstName;
     private String lastName;
     private int age;
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public int getAge() { return age; }
+    public Long getId() { return id; }
+
+    public User(Long id, String firstName, String lastName, int age) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    // Сеттеры (если нужны)
+    public void setFirstName(String firstName) { this.firstName = firstName; }
 }
